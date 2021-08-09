a08df6e8a4e19ec49f81fd137a94cc21c17716bc
wujiahua <wujiahua_lisp@outlook.com>
2021-08-09 22:56:58 +0800

    first unique char

---
+++ src/main/java/io/been/LeetCode387.java
@@ -0,0 +1,21 @@
+package io.been;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode387 {
+    public int firstUnique(String s) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (counts.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
