545fb29de8ae1ae0a2d4f46e462d538b202ef18c
wujiahua <wujiahua_lisp@outlook.com>
2021-08-10 23:59:13 +0800

    reverse kgroup

---
+++ src/main/java/io/been/Leetcode25.java
@@ -0,0 +1,27 @@
+package io.been;
+
+public class Leetcode25 {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode a = head;
+        ListNode b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) return head;
+            b = b.next;
+        }
+        ListNode node = reverse(a, b);
+        a.next = reverseKGroup(b, k);
+        return node;
+    }
+
+    private ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre = null;
+        ListNode cur = a;
+        while (cur != b) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
