8a446a52cdc9f2ac32cf4a96de21bc86864e2e10
wujiahua <wujiahua_lisp@outlook.com>
2021-08-12 23:40:54 +0800

    remove duplicates

---
+++ src/main/java/io/been/Leetcode26.java
@@ -0,0 +1,16 @@
+package io.been;
+
+public class Leetcode26 {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow + 1;
+    }
+}
