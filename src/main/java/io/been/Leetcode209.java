c0b82290b944a9323835e8f70c0be0b3789b978c
wujiahua <wujiahua_lisp@outlook.com>
2021-08-13 20:24:08 +0800

    min sub array len

---
+++ src/main/java/io/been/Leetcode209.java
@@ -0,0 +1,42 @@
+package io.been;
+
+public class Leetcode209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int right = 0;
+        int res = Integer.MAX_VALUE;
+        while (true) {
+            if (right == nums.length && left == nums.length) {
+                break;
+            }
+            if (sum == target) {
+                res = Math.min(right - left, res);
+                if (right < nums.length) {
+                    sum += nums[right];
+                    right++;
+                } else {
+                    sum -= nums[left];
+                    left++;
+                }
+            } else if (sum < target) {
+                if (right == nums.length) {
+                    break;
+                }
+                sum += nums[right];
+                right++;
+            } else {
+                res = Math.min(right - left, res);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        int index = new Leetcode209().minSubArrayLen(7, nums);
+        System.out.println(index);
+    }
+}
