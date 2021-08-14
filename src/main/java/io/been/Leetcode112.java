f848169f63ae4dda71bb894fad9dd15b1756f027
wujiahua <wujiahua_lisp@outlook.com>
2021-08-14 13:31:22 +0800

    has path sum

---
+++ src/main/java/io/been/Leetcode112.java
@@ -0,0 +1,18 @@
+package io.been;
+
+public class Leetcode112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        return visit(root, targetSum, 0);
+    }
+
+    private boolean visit(TreeNode node, int target, int curr) {
+        if (node == null) return false;
+        if (node.left == null && node.right == null) {
+            return (curr + node.val == target);
+        } else {
+            return visit(node.left, target, curr + node.val) ||
+                    visit(node.right, target, curr + node.val);
+        }
+    }
+}
