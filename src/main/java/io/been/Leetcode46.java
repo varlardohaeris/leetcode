0a7948288b6d7cc40f5a0bda8477730c37108159
wujiahua <wujiahua_lisp@outlook.com>
2021-08-14 20:19:03 +0800

    permute

---
+++ src/main/java/io/been/Leetcode46.java
@@ -0,0 +1,32 @@
+package io.been;
+
+import java.util.*;
+
+public class Leetcode46 {
+    private List<List<Integer>> res = new ArrayList<>();
+    private boolean[] visited;
+
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        visited = new boolean[nums.length];
+        backtrack(nums, track);
+        return res;
+    }
+
+    private void backtrack(int[] nums, LinkedList<Integer> track) {
+        if (track.size() == nums.length) {
+            res.add(new ArrayList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            track.add(nums[i]);
+            backtrack(nums, track);
+            visited[i] = false;
+            track.removeLast();
+        }
+    }
+}
