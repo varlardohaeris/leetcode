51ede5d489c05dc40c2dd79d07e188a3a83f9390
wujiahua <wujiahua_lisp@outlook.com>
2021-08-14 23:31:39 +0800

    inorder

---
+++ src/main/java/io/been/Leetcode94.java
@@ -0,0 +1,21 @@
+package io.been;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode94 {
+    private List<Integer> res = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return res;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node != null) {
+            inorder(node.left);
+            res.add(node.val);
+            inorder(node.right);
+        }
+    }
+}
