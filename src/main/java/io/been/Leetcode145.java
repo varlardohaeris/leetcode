f42487a8f7f9c2e25f29c4975788ca3678c7dded
wujiahua <wujiahua_lisp@outlook.com>
2021-08-14 23:34:41 +0800

    post order

---
+++ src/main/java/io/been/Leetcode145.java
@@ -0,0 +1,21 @@
+package io.been;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode145 {
+    private List<Integer> res = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return res;
+    }
+
+    private void postorder(TreeNode node) {
+        if (node != null) {
+            postorder(node.left);
+            postorder(node.right);
+            res.add(node.val);
+        }
+    }
+}
