5546cf3db18d87fbd0e02295799d8f46362e114e
wujiahua <wujiahua_lisp@outlook.com>
2021-08-14 15:29:34 +0800

    has cycle

---
+++ src/main/java/io/been/Leetcode141.java
@@ -0,0 +1,14 @@
+package io.been;
+
+public class Leetcode141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
