f600bce2b04cc844078cef7d9e246563ccf2a04a
wujiahua <wujiahua_lisp@outlook.com>
2021-08-15 23:10:52 +0800

    invert tree

---
+++ src/main/java/io/been/Leetcode226.java
@@ -0,0 +1,13 @@
+package io.been;
+
+public class Leetcode226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode node = root.left;
+        root.left = root.right;
+        root.right  = node;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
