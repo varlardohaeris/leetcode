351817196054001db33c33acc0d3dd823577500d
wujiahua <wujiahua_lisp@outlook.com>
2021-08-15 13:36:59 +0800

    sorted array to bst

---
+++ src/main/java/io/been/Leetcode108.java
@@ -0,0 +1,19 @@
+package io.been;
+
+public class Leetcode108 {
+    private int[] nums;
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        this.nums = nums;
+        return build(0, nums.length - 1);
+    }
+
+    private TreeNode build(int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = build(start, mid - 1);
+        node.right = build(mid + 1, end);
+        return node;
+    }
+}
