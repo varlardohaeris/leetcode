377c5dc4aed0f4fb27682670156636c06bc54ccb
wujiahua <wujiahua_lisp@outlook.com>
2021-08-15 19:58:59 +0800

    fib

---
+++ src/main/java/io/been/Leetcode509.java
@@ -0,0 +1,16 @@
+package io.been;
+
+public class Leetcode509 {
+    public int fib(int n) {
+        if (n <= 1) return n;
+        int dp0 = 0;
+        int dp1 = 1;
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            sum = dp0 + dp1;
+            dp0 = dp1;
+            dp1 = sum;
+        }
+        return sum;
+    }
+}
