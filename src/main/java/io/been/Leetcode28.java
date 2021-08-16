3eae921a405975af9bc5ccc9c58be3b30add31fa
wujiahua <wujiahua_lisp@outlook.com>
2021-08-16 22:52:32 +0800

    remove element

---
+++ src/main/java/io/been/Leetcode28.java
@@ -0,0 +1,19 @@
+package io.been;
+
+public class Leetcode28 {
+    public int strStr(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+        for (int i = 0; i < m - n + 1; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+
+            }
+            if (j == n) return i;
+        }
+        return -1;
+    }
+}
