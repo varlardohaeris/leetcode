ecb49ddead96ce24c9547208e5751124409f86c6
wujiahua <wujiahua_lisp@outlook.com>
2021-08-16 23:02:23 +0800

    swapPairs

---
+++ src/main/java/io/been/Leetcode24.java
@@ -0,0 +1,11 @@
+package io.been;
+
+public class Leetcode24 {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode new_head = head.next;
+        head.next = swapPairs(new_head.next);
+        new_head.next = head;
+        return new_head;
+    }
+}
