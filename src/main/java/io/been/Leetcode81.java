94345641e0cb4dcf9d1c44173efbd88334978d37
wujiahua <wujiahua_lisp@outlook.com>
2021-08-16 21:37:08 +0800

    find target in rotated array with duplicates

---
+++ src/main/java/io/been/Leetcode81.java
@@ -0,0 +1,28 @@
+package io.been;
+
+public class Leetcode81 {
+    public boolean search(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) return true;
+            if (nums[mid] < nums[i]) {
+                if (nums[mid] < target && nums[j] >= target) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            } else if (nums[mid] > nums[i]) {
+                if (nums[mid] > target && nums[i] <= target) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
