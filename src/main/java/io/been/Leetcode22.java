132e30e586ff798c63fb35f8b38490a3e0b67ad0
wujiahua <wujiahua_lisp@outlook.com>
2021-08-16 23:28:24 +0800

    generate parenthesis

---
+++ src/main/java/io/been/Leetcode22.java
@@ -0,0 +1,21 @@
+package io.been;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode22 {
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int l, int r, String cur) {
+        if (l < 0 || r < 0 || l > r) return;
+        if (l == 0 && r == 0) res.add(cur);
+        dfs(l - 1, r, cur + "(");
+        dfs(l, r - 1, cur + ")");
+    }
+
+}
