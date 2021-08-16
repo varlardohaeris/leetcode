33293364025c8ee4b811362f4deb266a02181705
wujiahua <wujiahua_lisp@outlook.com>
2021-08-16 23:50:32 +0800

    merge two list

---
+++ src/main/java/io/been/Leetcode21.java
@@ -0,0 +1,27 @@
+package io.been;
+
+public class Leetcode21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                cur.next = l2;
+                break;
+            } else if (l2 == null) {
+                cur.next = l1;
+                break;
+            }
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                cur = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = l2;
+                l2 = l2.next;
+            }
+        }
+        return dummy.next;
+    }
+}
