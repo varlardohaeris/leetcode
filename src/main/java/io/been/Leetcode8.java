f89aa0fe9af4a13113fd7c34ed82d759d1c3e183
wujiahua <wujiahua_lisp@outlook.com>
2021-08-17 23:31:39 +0800

    myatoi

---
+++ src/main/java/io/been/Leetcode8.java
@@ -0,0 +1,38 @@
+package io.been;
+
+public class Leetcode8 {
+    public int myAtoi(String s) {
+        if (s == null || s.equals("")) return 0;
+        while (s.length() > 0 && s.charAt(0) == ' ') {
+            s = s.substring(1);
+        }
+        if (s.length() == 0) return 0;
+        int sign = 1;
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
+            if (s.charAt(0) == '-') sign = -1;
+            s = s.substring(1);
+        }
+
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                break;
+            }
+            int v = s.charAt(i) - '0';
+            if (res > Integer.MAX_VALUE / 10) {
+                if (sign == 1) return Integer.MAX_VALUE;
+                if (sign == -1) return Integer.MIN_VALUE;
+            } else if (res == Integer.MAX_VALUE / 10) {
+                if (sign == 1 && v >= 7) return Integer.MAX_VALUE;
+                if (sign == -1 && v >= 8) return Integer.MIN_VALUE;
+            }
+            res = 10 * res + v;
+        }
+        return res * sign;
+    }
+
+    public static void main(String[] args) {
+        int res = new Leetcode8().myAtoi(" ");
+        System.out.println(res);
+    }
+}
