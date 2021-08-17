f39be58423d40e1e5f7668a4aac346fd950a65e2
wujiahua <wujiahua_lisp@outlook.com>
2021-08-17 23:14:17 +0800

    regular expression matching

---
+++ src/main/java/io/been/Leetcode10.java
@@ -0,0 +1,24 @@
+package io.been;
+
+public class Leetcode10 {
+    public boolean isMatch(String s, String p) {
+        return isMatch(s, 0, p, 0);
+    }
+
+    private boolean isMatch(String s, int idx1, String p, int idx2) {
+        if (idx2 >= p.length()) {
+            return idx1 == s.length();
+        }
+        if ((idx2 + 1) < p.length() && p.charAt(idx2 + 1) == '*') {
+            if (isMatch(s, idx1, p, idx2 + 2)) {
+                return true;
+            }
+            if (idx1 < s.length() && (p.charAt(idx2) == '.' || s.charAt(idx1) == p.charAt(idx2))) {
+                return isMatch(s, idx1 + 1, p, idx2);
+            }
+        } else if (idx1 < s.length() && (p.charAt(idx2) == '.' || s.charAt(idx1) == p.charAt(idx2))) {
+            return isMatch(s, idx1 + 1, p, idx2 + 1);
+        }
+        return false;
+    }
+}
