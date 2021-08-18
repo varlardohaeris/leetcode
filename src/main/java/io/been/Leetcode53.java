12883067d83226de35f7789f62266539b88cd9ed
wujiahua <wujiahua_lisp@outlook.com>
2021-08-18 21:55:31 +0800

    max sub array

---
+++ src/main/java/io/been/Leetcode53.java
@@ -0,0 +1,14 @@
+package io.been;
+
+public class Leetcode53 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(dp[i], ans);
+        }
+        return ans;
+    }
+}
