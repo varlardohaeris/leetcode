63399c0aeacdf9b978e1d379b3867ad19bfeae58
wujiahua <wujiahua_lisp@outlook.com>
2021-08-18 22:38:23 +0800

    edit distance

---
+++ src/main/java/io/been/Leetcode72.java
@@ -0,0 +1,24 @@
+package io.been;
+
+public class Leetcode72 {
+    public int minDistance(String word1, String word2) {
+        int l1 = word1.length();
+        int l2 = word2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+        for (int i = 0; i <= l1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= l2; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                int c = (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1);
+                dp[i][j] = Math.min(dp[i - 1][j - 1] + c,
+                        Math.min(dp[i][j - 1], dp[i - 1][j]) + 1);
+            }
+        }
+        return dp[l1][l2];
+    }
+}
