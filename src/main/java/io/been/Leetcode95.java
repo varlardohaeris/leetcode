1b058a1968ff1515847e2a9f3b742844f7404552
wujiahua <wujiahua_lisp@outlook.com>
2021-08-19 22:29:32 +0800

    unique bianry seach trees

---
+++ src/main/java/io/been/Leetcode95.java
@@ -0,0 +1,34 @@
+package io.been;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode95 {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generate(1, n);
+    }
+
+    private List<TreeNode> generate(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> l = generate(start, i - 1);
+            List<TreeNode> r = generate(i + 1, end);
+            for (TreeNode left : l) {
+                for (TreeNode right : r) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
