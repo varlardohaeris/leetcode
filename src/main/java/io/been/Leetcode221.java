d15860ed57cd473fadcc8ebe873111bc73698623
wujiahua <wujiahua_lisp@outlook.com>
2021-08-20 23:19:36 +0800

    maximal square

---
+++ src/main/java/io/been/Leetcode221.java
@@ -0,0 +1,21 @@
+package io.been;
+
+public class Leetcode221 {
+    public int maximalSquare(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int max = 0;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],
+                            Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
