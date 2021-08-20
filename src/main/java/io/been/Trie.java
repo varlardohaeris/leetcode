05eaed21d305f8590c2d1757a5b8fe76a67635d7
wujiahua <wujiahua_lisp@outlook.com>
2021-08-20 23:06:38 +0800

    implement trie

---
+++ src/main/java/io/been/Trie.java
@@ -0,0 +1,52 @@
+package io.been;
+
+public class Trie {
+    private TrieNode root;
+
+    class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public String item = "";
+
+        public TrieNode() {
+
+        }
+    }
+
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = this.root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.item = word;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = this.root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.item.equals(word);
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = this.root;
+        for (char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
