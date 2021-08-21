f69af20089e45a30b2883e2a66e32f42e14b01b4
wujiahua <wujiahua_lisp@outlook.com>
2021-08-21 14:34:41 +0800

    palindromic substrings

---
+++ src/main/java/io/been/Leetcode647.java
@@ -0,0 +1,32 @@
+package io.been;
+
+public class Leetcode647 {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            res++;
+        }
+        for (int i= 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                res++;
+            }
+        }
+
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i+1][j - 1];
+                    if (dp[i][j]) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
