8dc314790c551733a0ce96b07682cd49703d0be0
wujiahua <wujiahua_lisp@outlook.com>
2021-08-21 14:30:57 +0800

    least interval

---
+++ src/main/java/io/been/Leetcode621.java
@@ -0,0 +1,19 @@
+package io.been;
+
+import java.util.Arrays;
+
+public class Leetcode621 {
+    public int leastInterval(char[] tasks, int n) {
+        int[] record = new int[26];
+        for (char task : tasks) {
+            record[task - 'A']++;
+        }
+        Arrays.sort(record);
+        int max_n = record[25] - 1;
+        int space = max_n * n;
+        for (int i = 24; i >= 0 && record[i] > 0; i--) {
+            space -= Math.min(max_n, record[i]);
+        }
+        return space > 0 ? tasks.length + space : tasks.length;
+    }
+}
