f1def6759c167204c79317138b978204bd62398f
wujiahua <wujiahua_lisp@outlook.com>
2021-08-21 11:44:42 +0800

    move zeros

---
+++ src/main/java/io/been/Leetcode283.java
@@ -0,0 +1,17 @@
+package io.been;
+
+public class Leetcode283 {
+    public void moveZeroes(int[] nums) {
+        int l = 0;
+        int r = 0;
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                int temp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = temp;
+                l++;
+            }
+            r++;
+        }
+    }
+}
