0b0617ddd09814f9c62719b1020130ad2a43e961
wujiahua <wujiahua_lisp@outlook.com>
2021-08-22 13:40:16 +0800

    lowesst common-anncestor of a binary search tree

---
+++ src/main/java/io/been/Leetcode235.java
@@ -0,0 +1,13 @@
+package io.been;
+
+public class Leetcode235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
