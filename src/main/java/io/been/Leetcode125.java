c24b101312bd117e647d1294d51b378067c87487
wujiahua <wujiahua_lisp@outlook.com>
2021-08-23 22:15:32 +0800

    valid palindrome

---
+++ src/main/java/io/been/Leetcode125.java
@@ -0,0 +1,25 @@
+package io.been;
+
+public class Leetcode125 {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            while (start < end
+                    && !Character.isLetter(s.charAt(start))
+                    && !Character.isDigit(s.charAt(start))) {
+                start++;
+            }
+            while (start < end
+                    && !Character.isLetter(s.charAt(end))
+                    && !Character.isDigit(s.charAt(end))) {
+                end--;
+            }
+            if (s.charAt(start++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
