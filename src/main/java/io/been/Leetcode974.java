e82e1cdb52ba6500cb315d1fe5738ad0dddc6885
wujiahua <wujiahua_lisp@outlook.com>
2021-08-23 21:18:32 +0800

    subarray sums divisible by k

---
+++ src/main/java/io/been/Leetcode974.java
@@ -0,0 +1,19 @@
+package io.been;
+
+public class Leetcode974 {
+    public int subarraysDivByK(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        int[] freq = new int[k];
+        freq[0] = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                nums[i] += nums[i - 1];
+            }
+            int mod = nums[i] % k < 0 ? nums[i] % k + k : nums[i] % k;
+            count += freq[mod];
+            freq[mod]++;
+        }
+        return count;
+    }
+}
