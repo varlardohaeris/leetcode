742e290a1e933c1d3c51120cf88c084f35968a72
wujiahua <wujiahua_lisp@outlook.com>
2021-08-25 20:25:57 +0800

    smallest range covering elements

---
+++ src/main/java/io/been/Leetcode632.java
@@ -0,0 +1,51 @@
+package io.been;
+
+import java.util.List;
+import java.util.TreeSet;
+
+public class Leetcode632 {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        TreeSet<Elem> candidate = new TreeSet<Elem>((o1, o2)
+                -> Integer.compare(o1.val, o2.val) == 0
+                ? Integer.compare(o1.list, o2.list)
+                : Integer.compare(o1.val, o2.val)
+        );
+
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            Elem e = new Elem(i, 0, nums.get(i).get(0));
+            candidate.add(e);
+        }
+
+        int range = Integer.MAX_VALUE;
+        int[] res = new int[2];
+        while (candidate.size() == n) {
+            int max = candidate.last().val;
+            int min = candidate.first().val;
+            if (max - min < range) {
+                range = max - min;
+                res[0] = min;
+                res[1] = max;
+            }
+            Elem f = candidate.pollFirst();
+            if (f.idx + 1 < nums.get(f.list).size()) {
+                Elem next = new Elem(f.list, f.idx + 1,
+                        nums.get(f.list).get(f.idx + 1));
+                candidate.add(next);
+            }
+        }
+        return res;
+    }
+
+    class Elem {
+        int list;
+        int idx;
+        int val;
+
+        public Elem(int list, int idx, int val) {
+            this.list = list;
+            this.idx = idx;
+            this.val = val;
+        }
+    }
+}
