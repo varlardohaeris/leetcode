ec57a976ec70020ca11c07e1425da7e571e3a376
wujiahua <wujiahua_lisp@outlook.com>
2021-08-25 22:15:00 +0800

    binary tree level order traversal

---
+++ src/main/java/io/been/Leetcode107.java
@@ -0,0 +1,32 @@
+package io.been;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Leetcode107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.remove();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            res.add(0, level);
+        }
+        return res;
+    }
+}
