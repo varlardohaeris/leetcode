45cdf4af9df10833d6a1120eefbc091ee0eea3d6
wujiahua <wujiahua_lisp@outlook.com>
2021-08-25 21:34:43 +0800

    reverse bits

---
+++ src/main/java/io/been/Leetcode190.java
@@ -0,0 +1,15 @@
+package io.been;
+
+public class Leetcode190 {
+    public int reverseBits(int n) {
+        int res = 0;
+        int mask = 1;
+        int count = 32;
+        while (count-- > 0) {
+            res = res << 1;
+            res += (mask & n);
+            n >>= 1;
+        }
+        return res;
+    }
+}
