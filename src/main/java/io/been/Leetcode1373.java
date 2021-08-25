ef5db8e83e02f2f90b42f2a4dd475de90177f276
wujiahua <wujiahua_lisp@outlook.com>
2021-08-25 22:37:18 +0800

    maximum sum bst in binary tree

--- src/main/java/io/been/Leetcode1373.java
+++ src/main/java/io/been/Leetcode1373.java
@@ -1,2 +1,31 @@
-package io.been;public class Leetcode1373 {
+package io.been;
+
+public class Leetcode1373 {
+    private int res;
+
+    public int maxSumBST(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private int[] dfs(TreeNode cur) {
+        if (cur == null) return null;
+        int[] left = dfs(cur.left);
+        int[] right = dfs(cur.right);
+        int[] info = {1, cur.val, cur.val, cur.val};
+        if (left != null && (left[3] >= cur.val || left[0] == 0)
+                || right != null && (right[2] <= cur.val || right[0] == 0)) {
+            info[0] = 0;
+            return info;
+        }
+        info[1] += (left != null ? left[1] : 0) + (right != null ? right[1] : 0);
+        if (left != null) {
+            info[2] = left[2];
+        }
+        if (right != null) {
+            info[3] = right[3];
+        }
+        res = Math.max(res, info[1]);
+        return info;
+    }
 }
