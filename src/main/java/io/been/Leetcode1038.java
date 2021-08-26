90739a04444538c306889f25c69fef9fc215b5e8
wujiahua <wujiahua_lisp@outlook.com>
2021-08-26 23:09:35 +0800

    binary search tree to greater sum tree

---
+++ src/main/java/io/been/Leetcode1038.java
@@ -0,0 +1,20 @@
+package io.been;
+
+public class Leetcode1038 {
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        traversal(root);
+        return root;
+    }
+
+    private void traversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traversal(root.right);
+        sum += root.val;
+        root.val = sum;
+        traversal(root.left);
+    }
+}
