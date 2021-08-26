bb78bff70896676588ed1438be62c55f58ad0867
wujiahua <wujiahua_lisp@outlook.com>
2021-08-26 23:39:01 +0800

    delete node

--- src/main/java/io/been/Leetcode450.java
+++ src/main/java/io/been/Leetcode450.java
@@ -1,2 +1,24 @@
-package io.been;public class Leetcode450 {
+package io.been;
+
+public class Leetcode450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (root.val == key) {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            TreeNode min = getMin(root.right);
+            root.val = min.val;
+            root.right = deleteNode(root.right, min.val);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            root.right = deleteNode(root.right, key);
+        }
+        return root;
+    }
+
+    private TreeNode getMin(TreeNode node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
 }
