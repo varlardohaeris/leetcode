3af245afb05043f854b6dbf835f9963d130e8e24
wujiahua <wujiahua_lisp@outlook.com>
2021-09-04 23:04:39 +0800

    reorder list

---
+++ src/main/java/io/been/leetcode/Leetcode143.java
@@ -0,0 +1,40 @@
+package io.been.leetcode;
+
+public class Leetcode143 {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+        ListNode mid = findMid(head);
+        ListNode l2 = mid.next;
+        mid.next = null;
+        l2 = reverse(l2);
+        ListNode l1 = head;
+        while (l1 != null && l2 != null) {
+            ListNode next = l1.next;
+            l1.next = l2;
+            l2 = l2.next;
+            l1.next.next = next;
+            l1 = next;
+        }
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
