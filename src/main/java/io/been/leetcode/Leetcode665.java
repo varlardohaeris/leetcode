b89100f12a6cf58fa4741e58cc28cc80200ae094
wujiahua <wujiahua_lisp@outlook.com>
2021-09-04 23:32:20 +0800

    non decreasing array

---
+++ src/main/java/io/been/leetcode/Leetcode665.java
@@ -0,0 +1,21 @@
+package io.been.leetcode;
+
+public class Leetcode665 {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+                if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+        return true;
+    }
+}
