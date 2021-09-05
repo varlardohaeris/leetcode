a4aa96953088f9f07a85d80fe37cab3ac23839c8
wujiahua <wujiahua_lisp@outlook.com>
2021-09-05 21:09:03 +0800

    array parition I

---
+++ src/main/java/io/been/leetcode/Leetcode561.java
@@ -0,0 +1,16 @@
+package io.been.leetcode;
+
+import java.util.Arrays;
+
+public class Leetcode561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+}
