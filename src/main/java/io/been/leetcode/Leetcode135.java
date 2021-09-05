48357d77b6c3dd78bd2524f3f1ea47cb56f14454
wujiahua <wujiahua_lisp@outlook.com>
2021-09-05 20:33:13 +0800

    candy

---
+++ src/main/java/io/been/leetcode/Leetcode135.java
@@ -0,0 +1,22 @@
+package io.been.leetcode;
+
+import java.util.Arrays;
+
+public class Leetcode135 {
+    public int candy(int[] ratings) {
+        if (ratings.length == 0) return 0;
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+            }
+        }
+        return Arrays.stream(candy).sum();
+    }
+}
