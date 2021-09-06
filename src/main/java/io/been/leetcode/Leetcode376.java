340d1ceedb3e1df6e8637900bf7f7e7c1723d299
wujiahua <wujiahua_lisp@outlook.com>
2021-09-06 21:39:08 +0800

    wiggle subsequence

---
+++ src/main/java/io/been/leetcode/Leetcode376.java
@@ -0,0 +1,16 @@
+package io.been.leetcode;
+
+public class Leetcode376 {
+    public int wiggleMaxLength(int[] nums) {
+        int up = 1;
+        int down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+}
