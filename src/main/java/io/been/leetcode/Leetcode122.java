528bcc965823ce005fecf030980cf7cd7e5f75d5
wujiahua <wujiahua_lisp@outlook.com>
2021-09-06 23:41:36 +0800

    best time to buy and sell stock

---
+++ src/main/java/io/been/leetcode/Leetcode122.java
@@ -0,0 +1,13 @@
+package io.been.leetcode;
+
+public class Leetcode122 {
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                total += prices[i] - prices[i - 1];
+            }
+        }
+        return total;
+    }
+}
