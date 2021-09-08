4c85109ad43f284cc02a5ae69acaafcfdccf3513
wujiahua <wujiahua_lisp@outlook.com>
2021-09-08 22:12:34 +0800

    leetcode 647 : palindromic substrings
    https://www.cnblogs.com/grandyang/p/7404777.html

--- src/main/java/io/been/leetcode/Leetcode647.java
+++ src/main/java/io/been/leetcode/Leetcode647.java
@@ -1,32 +1,27 @@
 package io.been.leetcode;
 
 public class Leetcode647 {
-    public int countSubstrings(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            res++;
-        }
-        for (int i= 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                res++;
-            }
-        }
+    private String s;
+    private int res;
 
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i + len <= n; i++) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i+1][j - 1];
-                    if (dp[i][j]) {
-                        res++;
-                    }
-                }
-            }
+    public int countSubstrings(String s) {
+        if (s.length() == 0) return 0;
+        int n = s.length();
+        this.res = 0;
+        this.s = s;
+
+        for (int i = 0; i < n; i++) {
+            count(i, i);
+            count(i, i + 1);
         }
         return res;
+    }
+
+    private void count(int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            res++;
+            start--;
+            end++;
+        }
     }
 }
