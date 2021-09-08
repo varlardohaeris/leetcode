dfa2da2e94edd522ae5a4972e85fb92236c007b1
wujiahua <wujiahua_lisp@outlook.com>
2021-09-08 21:56:47 +0800

    leetcode132: min cut
    https://www.cnblogs.com/grandyang/p/4271456.html

--- src/main/java/io/been/leetcode/Leetcode132.java
+++ src/main/java/io/been/leetcode/Leetcode132.java
@@ -1,32 +1,21 @@
 package io.been.leetcode;
 
-import java.util.Arrays;
 
 public class Leetcode132 {
     public int minCut(String s) {
+        if (s.length() == 0) return 0;
         int n = s.length();
-        boolean[][] flag = new boolean[n][n];
+        boolean[][] p = new boolean[n][n];
+        int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(flag[i], true);
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-                flag[i][j] = (s.charAt(i) == s.charAt(j) && flag[i + 1][j - 1]);
-            }
-        }
-        int[] ans = new int[n];
-        Arrays.fill(ans, Integer.MAX_VALUE);
-        for (int i = 0; i < n; i++) {
-            if (flag[0][i]) {
-                ans[i] = 0;
-            } else {
-                for (int j = 0; j < i; j++) {
-                    if (flag[j + 1][i]) {
-                        ans[i] = Math.min(ans[i], ans[j] + 1);
-                    }
+            dp[i] = i;
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (i - j < 2 || p[j + 1][i - 1])) {
+                    p[j][i] = true;
+                    dp[i] = (j == 0) ? 0 : Math.min(dp[i], dp[j - 1] + 1);
                 }
             }
         }
-        return ans[n - 1];
+        return dp[n - 1];
     }
 }
