5a74c84cc439c0ce553c6ca5e1bd5c345e22e147
wujiahua <wujiahua_lisp@outlook.com>
2021-09-08 21:02:36 +0800

    leetcode 37: solve sodoku

--- src/main/java/io/been/leetcode/Leetcode37.java
+++ src/main/java/io/been/leetcode/Leetcode37.java
@@ -1,55 +1,49 @@
 package io.been.leetcode;
 
 public class Leetcode37 {
-    private int[][] rows_, cols_, boxes_;
+    private char[][] board;
 
     public void solveSudoku(char[][] board) {
-        this.rows_ = new int[9][10];
-        this.cols_ = new int[9][10];
-        this.boxes_ = new int[9][10];
-        for (int i = 0; i < 9; i++)
-            for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c != '.') {
-                    int n = c - '0';
-                    int bx = j / 3;
-                    int by = i / 3;
-                    rows_[i][n] = 1;
-                    cols_[j][n] = 1;
-                    boxes_[by * 3 + bx][n] = 1;
-                }
-            }
-
-        fill(board, 0, 0);
+        this.board = board;
+        backtrack(0, 0);
     }
 
-
-    private boolean fill(char[][] board, int x, int y) {
-        if (y == 9)
+    private boolean backtrack(int row, int col) {
+        if (col == 9) {
+            return backtrack(row + 1, 0);
+        }
+        if (row == 9) {
             return true;
+        }
 
-        int nx = (x + 1) % 9;
-        int ny = (nx == 0) ? y + 1 : y;
-
-        if (board[y][x] != '.') return fill(board, nx, ny);
-
-        for (int i = 1; i <= 9; i++) {
-            int bx = x / 3;
-            int by = y / 3;
-            int box_key = by * 3 + bx;
-            if (rows_[y][i] != 1 && cols_[x][i] != 1 && boxes_[box_key][i] != 1) {
-                rows_[y][i] = 1;
-                cols_[x][i] = 1;
-                boxes_[box_key][i] = 1;
-                board[y][x] = Character.forDigit(i, 10);
-                if (fill(board, nx, ny)) return true;
-                board[y][x] = '.';
-                boxes_[box_key][i] = 0;
-                cols_[x][i] = 0;
-                rows_[y][i] = 0;
+        if (board[row][col] != '.') {
+            return backtrack(row, col + 1);
+        }
+        for (char c = '1'; c <= '9'; c++) {
+            if (!isValid(row, col, c)) {
+                continue;
             }
+            board[row][col] = c;
+            if (backtrack(row, col + 1)) {
+                return true;
+            }
+            board[row][col] = '.';
         }
         return false;
     }
 
+    private boolean isValid(int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) {
+                return false;
+            }
+            if (board[i][col] == c) {
+                return false;
+            }
+            if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
