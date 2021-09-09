f76afdda3fa54b7eed6c2d3787cde93b6c313fff
wujiahua <wujiahua_lisp@outlook.com>
2021-09-09 23:14:43 +0800

    left bound and right bound for binary search

--- src/main/java/io/been/alg/BinarySearch.java
+++ src/main/java/io/been/alg/BinarySearch.java
@@ -2,32 +2,54 @@ package io.been.alg;
 
 public class BinarySearch {
     public static int binarySearch(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
-                end = mid - 1;
+                right = mid - 1;
             } else {
-                start = mid + 1;
+                left = mid + 1;
             }
         }
         return -1;
     }
 
+    public static int leftBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                right = mid;
+            } else if (target < nums[mid]) {
+                right = mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static int rightBound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (target >= nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left - 1;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-        int idx = binarySearch(nums, 1);
-        System.out.println(idx);
-        idx = binarySearch(nums, 2);
-        System.out.println(idx);
-        idx = binarySearch(nums, 3);
-        System.out.println(idx);
-        idx = binarySearch(nums, 4);
-        System.out.println(idx);
-        idx = binarySearch(nums, 5);
-        System.out.println(idx);
+        int[] nums = {1, 3, 3, 3, 3, 3, 3, 5};
+        leftBound(nums, 3);
+        rightBound(nums, 3);
     }
 }
