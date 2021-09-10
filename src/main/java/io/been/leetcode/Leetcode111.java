7951563f94ecbf5f8f09f1999762489737af9daf
wujiahua <wujiahua_lisp@outlook.com>
2021-09-10 20:35:09 +0800

    leetcode 111: minimum depth of binary tree -- bfs

--- src/main/java/io/been/leetcode/Leetcode111.java
+++ src/main/java/io/been/leetcode/Leetcode111.java
@@ -1,16 +1,32 @@
 package io.been.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Leetcode111 {
     public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
-        int ans = Integer.MAX_VALUE;
-        if (root.left != null) {
-            ans = Math.min(minDepth(root.left), ans);
+        if (root == null) {
+            return 0;
         }
-        if (root.right != null) {
-            ans = Math.min(minDepth(root.right), ans);
+        int depth = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
         }
-        return ans + 1;
+        return depth;
     }
 }
