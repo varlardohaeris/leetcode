ddec3b7416289bb2be50dd8b80e7a5a329e3f68a
wujiahua <wujiahua_lisp@outlook.com>
2021-09-10 20:06:08 +0800

    leetcode 438: find all angrams in a string, with sliding window

--- src/main/java/io/been/leetcode/Leetcode438.java
+++ src/main/java/io/been/leetcode/Leetcode438.java
@@ -1,48 +1,42 @@
 package io.been.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Leetcode438 {
     public List<Integer> findAnagrams(String s, String p) {
-        int start = 0;
-        int left = 0;
-        int right = 0;
-        int match = 0;
         List<Integer> res = new ArrayList<>();
-        Map<Character, Integer> window = new HashMap<>();
-        Map<Character, Integer> needs = new HashMap<>();
+        int[] window = new int[26];
+        int[] needs = new int[26];
+        Set<Character> set = new HashSet<>();
         for (char c : p.toCharArray()) {
-            needs.put(c, needs.getOrDefault(c, 0) + 1);
+            needs[c - 'a']++;
+            set.add(c);
         }
 
+        int left = 0;
+        int right = 0;
+        int valid = 0;
         while (right < s.length()) {
-            char c1 = s.charAt(right);
-            if (needs.containsKey(c1)) {
-                window.put(c1, window.getOrDefault(c1, 0) + 1);
-                if (window.get(c1).equals(needs.get(c1))) {
-                    match++;
-                }
-            } else {
-                left = right + 1;
-                window = new HashMap<>();
-                match = 0;
-            }
+            char c = s.charAt(right);
             right++;
-
-            while (match == needs.size()) {
-                start = left;
-                if (window.equals(needs)) res.add(start);
-                char c2 = s.charAt(left);
-                if (needs.containsKey(c2)) {
-                    window.put(c2, window.get(c2) - 1);
-                    if (window.get(c2) < needs.get(c2)) {
-                        match--;
-                    }
+            if (needs[c - 'a'] > 0) {
+                window[c - 'a']++;
+                if (window[c - 'a'] == needs[c - 'a']) {
+                    valid++;
                 }
+            }
+            while (right - left >= p.length()) {
+                if (valid == set.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
                 left++;
+                if (needs[d - 'a'] > 0) {
+                    if (window[d - 'a'] == needs[d - 'a']) {
+                        valid--;
+                    }
+                    window[d - 'a']--;
+                }
             }
         }
         return res;
