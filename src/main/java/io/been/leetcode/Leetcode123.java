1ab2608eb12611ea223f45e72e3445211d310ff3
wujiahua <wujiahua_lisp@outlook.com>
2021-09-13 22:47:47 +0800

    leetcode 123: best time to buy and sell stokc III, dp

--- src/main/java/io/been/leetcode/Leetcode123.java
+++ src/main/java/io/been/leetcode/Leetcode123.java
@@ -2,25 +2,17 @@ package io.been.leetcode;
 
 public class Leetcode123 {
     public int maxProfit(int[] prices) {
-        int firstBuy = Integer.MIN_VALUE;
-        int firstSell = 0;
-        int secondBuy = Integer.MIN_VALUE;
-        int secondSell = 0;
-        for (int price : prices) {
-            if (firstBuy < -price) {
-                firstBuy = -price;
+        if (prices.length == 0) return 0;
+        int n = prices.length;
+        int[][] global = new int[n][3];
+        int[][] local = new int[n][3];
+        for (int i = 1; i < n; i++) {
+            int diff = prices[i] - prices[i - 1];
+            for (int j = 1; j <= 2; j++) {
+                local[i][j] = Math.max(global[i - 1][j - 1] + Math.max(diff, 0), local[i - 1][j] + diff);
+                global[i][j] = Math.max(local[i][j], global[i - 1][j]);
             }
-            if (firstSell < firstBuy + price) {
-                firstSell = firstBuy + price;
-            }
-            if (secondBuy < firstSell - price) {
-                secondBuy = firstSell - price;
-            }
-            if (secondSell < secondBuy + price) {
-                secondSell = secondBuy + price;
-            }
-
         }
-        return secondSell;
+        return global[n - 1][2];
     }
 }
