053f4ebcb120b9787fbf2fdb3568b12fda83d431
wujiahua <wujiahua_lisp@outlook.com>
2021-09-13 23:11:32 +0800

    leetcode 322: coin change

--- src/main/java/io/been/leetcode/Leetcode322.java
+++ src/main/java/io/been/leetcode/Leetcode322.java
@@ -5,17 +5,15 @@ import java.util.Arrays;
 public class Leetcode322 {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i && dp[i - coins[j]] != -1) {
-                    if (dp[i] == -1 || dp[i] > dp[i - coins[j]] + 1) {
-                        dp[i] = dp[i - coins[j]] + 1;
-                    }
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        return dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
