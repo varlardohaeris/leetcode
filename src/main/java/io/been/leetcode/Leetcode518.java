a6ccaa78a1334867138977bc9075a6b2bd59360f
wujiahua <wujiahua_lisp@outlook.com>
2021-09-13 23:00:59 +0800

    leetcode 518: coin change II

--- src/main/java/io/been/leetcode/Leetcode518.java
+++ src/main/java/io/been/leetcode/Leetcode518.java
@@ -4,16 +4,11 @@ public class Leetcode518 {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int[][] dp = new int[n + 1][amount + 1];
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
+        dp[0][0] = 1;
         for (int i = 1; i <= n; i++) {
+            dp[i][0] = 1;
             for (int j = 1; j <= amount; j++) {
-                dp[i][j] += dp[i - 1][j];
-                if (j >= coins[i - 1]) {
-                    dp[i][j] += dp[i][j - coins[i - 1]];
-                }
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
             }
         }
         return dp[n][amount];
