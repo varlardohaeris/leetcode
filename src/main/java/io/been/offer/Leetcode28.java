a7109ed9b852e6ad88ef3607daf7e3964c13e5da
wujiahua <wujiahua_lisp@outlook.com>
2021-10-07 23:02:55 +0800

    offer 28: is Symmetric

---
+++ src/main/java/io/been/offer/Leetcode28.java
@@ -0,0 +1,20 @@
+package io.been.offer;
+
+import io.been.leetcode.TreeNode;
+
+public class Leetcode28 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right)
+                && isSymmetric(left.right, right.left);
+    }
+}
