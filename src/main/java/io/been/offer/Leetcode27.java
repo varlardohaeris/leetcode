1b09184b4b60a84a0e574f404b2250f4c5ee12f7
wujiahua <wujiahua_lisp@outlook.com>
2021-10-07 22:55:41 +0800

    offer 27: mirrorTree

---
+++ src/main/java/io/been/offer/Leetcode27.java
@@ -0,0 +1,16 @@
+package io.been.offer;
+
+import io.been.leetcode.TreeNode;
+
+public class Leetcode27 {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
