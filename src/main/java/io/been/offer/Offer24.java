149922926b4d8ca0122f0b6214e03342a22d3d8f
wujiahua <wujiahua_lisp@outlook.com>
2021-10-08 20:15:18 +0800

    offer 24: reverseList

---
+++ src/main/java/io/been/offer/Offer24.java
@@ -0,0 +1,15 @@
+package io.been.offer;
+
+import io.been.leetcode.ListNode;
+
+public class Offer24 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return cur;
+    }
+}
