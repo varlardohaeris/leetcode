deea66cf17db0aff175f07a4597b219d727268e7
wujiahua <wujiahua_lisp@outlook.com>
2021-10-08 19:58:53 +0800

    offer 5: replace space

---
+++ src/main/java/io/been/offer/Offer5.java
@@ -0,0 +1,15 @@
+package io.been.offer;
+
+public class Offer5 {
+    public String replaceSpace(String s) {
+        StringBuilder builder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                builder.append("%20");
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+}
