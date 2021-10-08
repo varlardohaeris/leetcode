fd893d5894bf9d3e08291082aea9e182459a5c7b
wujiahua <wujiahua_lisp@outlook.com>
2021-10-08 20:22:36 +0800

    offer 6: reverse print list

---
+++ src/main/java/io/been/offer/Offer6.java
@@ -0,0 +1,22 @@
+package io.been.offer;
+
+import io.been.leetcode.ListNode;
+
+import java.util.Stack;
+
+public class Offer6 {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode cur = head;
+        while (cur != null) {
+            stack.push(cur.val);
+            cur = cur.next;
+        }
+        int size = stack.size();
+        int[] res = new int[size];
+        for (int i = 0; i < size; i++) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
