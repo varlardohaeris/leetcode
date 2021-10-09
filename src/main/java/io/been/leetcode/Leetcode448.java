7125e88bf54568615add6b82841d93a6043b04b3
wujiahua <wujiahua_lisp@outlook.com>
2021-10-09 20:21:31 +0800

    leetcode 448

--- src/main/java/io/been/leetcode/Leetcode448.java
+++ src/main/java/io/been/leetcode/Leetcode448.java
@@ -2,28 +2,16 @@ package io.been.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// https://www.cnblogs.com/grandyang/p/6222149.html
 public class Leetcode448 {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int len = nums.length;
-        int index = 0;
-        while (index < len) {
-            if (nums[index] == index + 1) {
-                index++;
-            } else {
-                int targetIndex = nums[index] - 1;
-                if (nums[targetIndex] == nums[index]) {
-                    index++;
-                    continue;
-                }
-                int temp = nums[targetIndex];
-                nums[targetIndex] = nums[index];
-                nums[index] = temp;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            nums[idx] = (nums[idx] > 0) ? -nums[idx] : nums[idx];
         }
         List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < len; i++) {
-            if (nums[i] != (i + 1)) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
                 res.add(i + 1);
             }
         }
