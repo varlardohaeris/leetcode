9f4906da210c0d0331ccf5227d8de74eec26735b
wujiahua <wujiahua_lisp@outlook.com>
2021-11-03 20:57:04 +0800

    leetcode 875

--- src/main/java/io/been/leetcode/Leetcode875.java
+++ src/main/java/io/been/leetcode/Leetcode875.java
@@ -3,19 +3,36 @@ package io.been.leetcode;
 public class Leetcode875 {
     public int minEatingSpeed(int[] piles, int h) {
         int left = 1;
-        int right = 1000000000;
+        int right = getMax(piles) + 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            int cnt = 0;
-            for (int pile : piles) {
-                cnt += (pile + mid - 1) / mid;
-            }
-            if (cnt > h) {
-                left = mid + 1;
-            } else {
+            if (canFinish(piles, mid, h)) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return right;
+        return left;
     }
+
+    private boolean canFinish(int[] piles, int speed, int h) {
+        int time = 0;
+        for (int n : piles) {
+            time += speedOf(n, speed);
+        }
+        return time <= h;
+    }
+
+    private int speedOf(int n, int speed) {
+        return (n / speed) + (((n % speed) > 0) ? 1 : 0);
+    }
+
+    private int getMax(int[] piles) {
+        int res = piles[0];
+        for (int i = 0; i < piles.length; i++) {
+            res = Math.max(piles[i], res);
+        }
+        return res;
+    }
+
 }
