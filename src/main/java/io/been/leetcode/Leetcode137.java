96f7bfaa03f163aa4fc73e68accabe1cf188967d
wujiahua <wujiahua_lisp@outlook.com>
2021-11-06 22:15:13 +0800

    leetcode 137: single number

---
+++ src/main/java/io/been/leetcode/Leetcode137.java
@@ -0,0 +1,17 @@
+package io.been.leetcode;
+
+public class Leetcode137 {
+    public int singleNumber(int[] nums) {
+        int one = 0;
+        int two = 0;
+        int three = 0;
+        for (int i = 0; i < nums.length; i++) {
+            two |= one & nums[i];
+            one ^= nums[i];
+            three = one & two;
+            one &= ~three;
+            two &= ~three;
+        }
+        return one;
+    }
+}
