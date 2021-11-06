0446af50f1842cd6f5448d9858c3f706d7865551
wujiahua <wujiahua_lisp@outlook.com>
2021-11-06 19:45:35 +0800

    leetcode 34: bianry search for left bound and right bound

--- src/main/java/io/been/leetcode/Leetcode34.java
+++ src/main/java/io/been/leetcode/Leetcode34.java
@@ -1,44 +1,54 @@
 package io.been.leetcode;
 
+
 public class Leetcode34 {
     public int[] searchRange(int[] nums, int target) {
         if (nums == null || nums.length == 0) return new int[]{-1, -1};
-        int start = findFirst(nums, target);
+        int start = left_bound(nums, target);
         if (start == -1) return new int[]{-1, -1};
-        int end = findLast(nums, target);
+        int end = right_bound(nums, target);
         if (end == -1) return new int[]{-1, -1};
         return new int[]{start, end};
     }
 
-    private int findFirst(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] < target) {
-                start = mid;
-            } else {
-                end = mid;
+    // ref
+    // https://www.bilibili.com/video/BV1wy4y1k76F?from=search&seid=17324666713374074520&spm_id_from=333.337.0.0
+    private int left_bound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] < target) {
+                    return mid;
+                }
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
-        if (nums[start] == target) return start;
-        if (nums[end] == target) return end;
         return -1;
     }
 
-    private int findLast(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > target) {
-                end = mid;
+    private int right_bound(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] > target) {
+                    return mid;
+                }
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                start = mid;
+                left = mid + 1;
             }
         }
-        if (nums[end] == target) return end;
-        if (nums[start] == target) return start;
         return -1;
     }
+
 }
