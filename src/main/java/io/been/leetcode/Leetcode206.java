e4272185c6b1d3730fb57372a8bc2a5d143890bc
wujiahua <wujiahua_lisp@outlook.com>
2021-11-06 21:22:01 +0800

    leetcode 206: reverse with dummy node

--- src/main/java/io/been/leetcode/Leetcode206.java
+++ src/main/java/io/been/leetcode/Leetcode206.java
@@ -13,6 +13,18 @@ public class Leetcode206 {
         return pre;
     }
 
+    public ListNode reverse(ListNode head) {
+        ListNode temp = new ListNode(-1);
+        temp.next = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = temp.next;
+            temp.next = head;
+            head = next;
+        }
+        return temp.next;
+    }
+
     public static void main(String[] args) {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
         ListNode res = new Leetcode206().reverseList(head);
